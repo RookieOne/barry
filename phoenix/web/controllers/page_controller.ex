f70160108ca964518a2269a01809ca806d2b381d
Jonathan Birkholz <rookieone@gmail.com>
2017-01-04 16:38:32 -0600

    Create styleguide page

--- phoenix/web/controllers/page_controller.ex
+++ phoenix/web/controllers/page_controller.ex
@@ -4,4 +4,10 @@ defmodule Barry.PageController do
   def index(conn, _params) do
     render conn, "index.html"
   end
+
+  def styleguide(conn, _params) do
+    conn
+    |> put_layout("styleguide.html")
+    |> render("styleguide.html")
+  end
 end
--- phoenix/web/router.ex
+++ phoenix/web/router.ex
@@ -17,6 +17,7 @@ defmodule Barry.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    get "/styleguide", PageController, :styleguide
     resources "/flash_cards", FlashCardsController
   end
 
